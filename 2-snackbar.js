8ccd7393d6e16b38333ad5e2a13873d75d46663f
KateChmil <chmilkatecat@gmail.com>
2024-11-30 20:44:04 +0000

    Deploying to gh-pages from @ KateChmil/goit-js-hw-10-2@48fac7a6aed9a2c60ad60807cfa39563b97be839 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import"./assets/vendor-BbbuE1sJ.js";const t=document.querySelector(".form");document.querySelector('input[name="delay"]');document.querySelector('input[name="state"]');t.addEventListener("submit",e=>{e.preventDefault()});
+//# sourceMappingURL=2-snackbar.js.map
